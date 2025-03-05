7dc6eb13e972af0e4505e152ca0db546bcb22df5
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-05 09:58:55 +0530

    use react-ruter-dom

--- myapp/src/App.jsx
+++ myapp/src/App.jsx
@@ -1,15 +1,27 @@
 import Home from "./components/Home";
 import About from "./components/About";
+import Navbar from "./components/Navbar";
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 
-function App(){
+function App() {
 
   return (
     <>
-      <h1>Hello</h1>
-      <hr />
-      <Home/>
-      <hr />
-      <About/>
+
+      <BrowserRouter>
+        <Navbar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </BrowserRouter>
+
+
+
+
+
+
+
     </>
   )
 }
