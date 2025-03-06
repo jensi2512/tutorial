411e1fff3cf83cccf63f77f47075449691046220
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-06 10:41:50 +0530

    useState

--- myapp2/src/components/About.jsx
+++ myapp2/src/components/About.jsx
@@ -1,9 +1,29 @@
-
+import { useState } from "react"
 
 function About() {
+
+    const [title, setTitle] = useState("Hii")
+    const [color, setCoolor] = useState(true)
+
+    function handleClick(){
+       if(title === "Hii"){
+        setTitle("Hello")
+       }else{
+        setTitle("Hii")
+       }
+    }
+    
+    function handleColor(){
+        setCoolor(color ? false : true)
+    }
+
   return (
     <div>
       <h1>About</h1>
+      <h1>{title}</h1>
+      <button className="btn btn-primary" onClick={handleClick}>Click</button>
+      <button onClick={handleColor}>Change Color</button>
+      <div className="box" style={color ? {backgroundColor : "blue"} : {backgroundColor : "red"}}></div>
     </div>
   )
 }
