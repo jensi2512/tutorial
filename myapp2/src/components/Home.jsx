a50e80ec86181159fcc460e03bd0be52db8b6867
jensi2512 <jensipatel19@gnu.ac.in>
2025-03-06 10:00:41 +0530

    props

--- myapp2/src/components/Home.jsx
+++ myapp2/src/components/Home.jsx
@@ -1,9 +1,24 @@
-
+import Table from "./Table"
 
 function Home() {
+    const user = {
+        name : "Hello",
+        age : 21
+      }
+
+      const username = "Hii"
+
+    function getData(arr){
+        console.log(arr)
+    }  
+    
   return (
     <div>
       <h1>Home</h1>
+      <h1>{user.name}</h1>
+      <h1>{user.age}</h1>
+      <hr />
+      <Table xyz={user} username={username} getData={getData}/>
     </div>
   )
 }
--- myapp2/src/components/Table.jsx
+++ myapp2/src/components/Table.jsx
@@ -1,9 +1,16 @@
 
 
-function Table() {
+function Table(abc) {
+    const {xyz, username, getData} = abc
+    const num = [1, 2, 3, 4, 5]
+    getData(num)
+
   return (
     <div>
-      
+      <h1>Table</h1>
+      <h1>{xyz.name}</h1>
+      <h1>{xyz.age}</h1>
+      <h1>{username}</h1>
     </div>
   )
 }
